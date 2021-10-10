f9c4c3bf8c4084dc61ce56c17b0796bbf4cf1504
baegofda <molly.daleo3o@gmail.com>
2021-10-10 18:41:55 +0900

    메타테크 추가

--- pages/detail/_id.vue
+++ pages/detail/_id.vue
@@ -25,10 +25,32 @@ export default {
     const { data: product } = await fetchProductById(params.id)
     return { product }
   },
+  head() {
+    return {
+      title: `쇼핑 아이템 - ${this.product.name}`,
+      meta: [
+        // 추가하고싶다면 hid를 다르게 ! 덮어씌우기라면 hid를 동일하게
+        {
+          hid: 'description',
+          name: 'description',
+          content: `이상품은 ${this.product.name} 입니다.`,
+        },
+        {
+          hid: 'og:title',
+          property: 'og:title',
+          content: `이상품은 ${this.product.name} 입니다.`,
+        },
+        {
+          hid: 'og:description',
+          property: 'og:description',
+          content: `이상품은 ${this.product.name} 입니다.`,
+        },
+      ],
+    }
+  },
   methods: {
     async addToCart() {
       const r = await createCartItem(this.product)
-      console.log(r)
       this.$store.commit('addCartItem', this.product)
       this.$router.push('/cart')
     },
